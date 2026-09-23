a0db99506278f2f6fc42f8477681fca513e51b75
Nikolas <gianick@users.noreply.github.com>
2026-09-23 12:05:33 +0100

    Script fixes

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_BROWSER=chrome
-BROWSER_TYPE=$1
-ENV=$2
+BROWSER=$1
+ENVIRONMENT=$2
 
-if [ -z "$BROWSER_TYPE" ]; then
-    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
-    echo ""
-fi
-
-sbt scalafmtCheckAll scalafmtSbtCheck clean -Dbrowser="chrome" -Denvironment="local" -Dbrowser.option.headless=true -Dbrowser.usePreviousVersion=true "testOnly uk.gov.hmrc.ui.specs.* -- -n AcceptanceTests" testReport
+sbt scalafmtCheckAll scalafmtSbtCheck clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless=true -Dbrowser.usePreviousVersion=true "testOnly uk.gov.hmrc.ui.specs.* -- -n AcceptanceTests" testReport
